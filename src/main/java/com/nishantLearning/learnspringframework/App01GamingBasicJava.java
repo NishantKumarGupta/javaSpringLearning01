7408b3a61b1448548bca90918691031b2d033fc0
Nishant Kumar Gupta <nishantg1210@gmail.com>
2023-03-09 18:51:32 +0530

    01 lesson complete

---
+++ src/main/java/com/nishantLearning/learnspringframework/App01GamingBasicJava.java
@@ -0,0 +1,16 @@
+package com.nishantLearning.learnspringframework;
+
+import com.nishantLearning.learnspringframework.game.GameRunner;
+import com.nishantLearning.learnspringframework.game.MarioGame;
+import com.nishantLearning.learnspringframework.game.PacmanGame;
+import com.nishantLearning.learnspringframework.game.SuperContraGame;
+
+public class App01GamingBasicJava {
+    public static void main(String[] args) {
+        var marioGame = new MarioGame();
+        var superContraGame = new SuperContraGame();
+        var pacmanGame = new PacmanGame();
+        var gameRunner = new GameRunner(pacmanGame);
+        gameRunner.run();
+    }
+}
---
+++ src/main/java/com/nishantLearning/learnspringframework/game/PacmanGame.java
@@ -0,0 +1,16 @@
+package com.nishantLearning.learnspringframework.game;
+
+public class PacmanGame implements GamingConsole {
+    public void up(){
+        System.out.println("Upar jaara hu");
+    }
+    public void down(){
+        System.out.println("Niche jaara hu");
+    }
+    public void left(){
+        System.out.println("Bhai guess kar kaha jaunga..left hi jaunga na");
+    }
+    public void right(){
+        System.out.println("Right :)");
+    }
+}
